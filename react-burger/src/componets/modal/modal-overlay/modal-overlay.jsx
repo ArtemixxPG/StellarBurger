ff90f930ecca4aabf3faa18ce8e7d2f1c9cbb877
ArtemixxPG <artemixpg@gmail.com>
2023-01-16 01:36:25 +0300

    add in step-2 branch

---
+++ react-burger/src/componets/modal/modal-overlay/modal-overlay.jsx
@@ -0,0 +1,10 @@
+
+import styles from './modal-overlay.module.scss'
+
+const ModalOverlay = () => {
+    return (
+        <div className={styles.modalOverlay}/>
+    );
+};
+
+export default ModalOverlay;
